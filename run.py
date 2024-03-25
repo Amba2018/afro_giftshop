692a864001fbcacb8d124e7183c02141f1e2d138
Emmanuel Manu <manu.emma.em@gmail.com>
2024-03-25 02:17:42 +0000

    Add comment to imports, scpoe and creds

--- run.py
+++ run.py
@@ -1,22 +1,23 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Settings and credentials to allow access, read and modify data in
+# Google Sheets
 import gspread
 from google.oauth2.service_account import Credentials
 
+# Scope for Google IAM auth for API program access
+# Guidance for setting up the APIs was provided by the Code Institute's
+# course material
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
     ]
 
+# const for untracked creds file
 CREDS = Credentials.from_service_account_file('creds.json')
+# const for credentials scope
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+# const to allow auth of gspread client within these scoped credentials
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+# const for google sgeet
 SHEET = GSPREAD_CLIENT.open('afro_giftshop')
 
-sales = SHEET.worksheet('sales')
-
-data = sales.get_all_values()
-
-print(data)
