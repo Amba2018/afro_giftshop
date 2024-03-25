a9fabad8e71a79150534cbeb759505a12360fcd1
Emmanuel Manu <manu.emma.em@gmail.com>
2024-03-25 02:37:59 +0000

    Add additional imports and test welcome ASCII

--- run.py
+++ run.py
@@ -3,6 +3,20 @@
 import gspread
 from google.oauth2.service_account import Credentials
 
+# time and sys libraries to add typing effect/delay
+import time
+import sys
+
+# os library to clear screen
+import os
+
+# colorama for text formatting
+import colorama
+from colorama import Fore, Back, Style
+
+# initialize colorama
+colorama.init(autoreset=True)
+
 # Scope for Google IAM auth for API program access
 # Guidance for setting up the APIs was provided by the Code Institute's
 # course material
@@ -21,3 +35,40 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 # const for google sgeet
 SHEET = GSPREAD_CLIENT.open('afro_giftshop')
 
+# typing effect function with delay effect for print and input
+# time.sleep() used as a pause effect with seconds parameter
+# clearScreen() to reset screen for new text
+def typePrint(text):
+    for character in text:
+        sys.stdout.write(character)
+        sys.stdout.flush()
+        time.sleep(0.05)
+
+def typeInput(text):
+    for character in text:
+        sys.stdout.write(character)
+        sys.stdout.flush()
+        time.sleep(0.05)
+    value = input()
+    return value
+
+def prog_start():
+   '''
+   Run opening screen for user and gives brief explanation of its use.
+   '''
+   print('''
+   
+                                                                             
+     .oo  d'b                .oPYo.  o  d'b   o         8                    
+    .P 8  8                  8    8     8     8         8                    
+   .P  8 o8P  oPYo. .oPYo.   8      o8 o8P   o8P .oPYo. 8oPYo. .oPYo. .oPYo. 
+  oPooo8  8   8  `' 8    8   8   oo  8  8     8  Yb..   8    8 8    8 8    8 
+ .P    8  8   8     8    8   8    8  8  8     8    'Yb. 8    8 8    8 8    8 
+.P     8  8   8     `YooP'   `YooP8  8  8     8  `YooP' 8    8 `YooP' 8YooP' 
+..:::::..:..::..:::::.....::::....8 :..:..::::..::.....:..:::..:.....:8 ....:
+::::::::::::::::::::::::::::::::::8 ::::::::::::::::::::::::::::::::::8 :::::
+::::::::::::::::::::::::::::::::::..::::::::::::::::::::::::::::::::::..:::::
+
+   ''')
+
+prog_start()
